94c81d85920f652ecdbdc2500aff5ea9275b0a01
Irina1234567890 <irina3213@gmail.com>
2022-07-17 11:06:36 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,3 +4,13 @@ import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+  searchBox: document.querySelector('#search-box'),
+  countryList: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.country-info')
+}
+
+refs.searchBox.addEventListener('input', debounce(onSearchInput, DEBOUNCE_DELAY));
+
+
